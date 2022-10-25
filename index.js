49fd68b0b9921bdd2c91d34cf1a0c10b77ce1b97
Dipta04 <diptakarmakar38@gmail.com>
2022-10-25 21:10:02 +0600

    server finish

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use (cors());
+
+const courses = require('./data/course.json')
+
+app.get('/courses', (req,res)=>{
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id==id)
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Courses running on the port ${port}`)
 })
